fb5bb0bc186526728199c9a66ff56c1f1f4903d5
Kristian Wahlroos <wakr@cs.helsinki.fi>
2015-05-11 14:11:07 +0300

    added initial test for coveralls

---
+++ src/test/java/hy/tmc/cli/MainTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hy.tmc.cli;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+public class MainTest {
+
+    @Test
+    public void genericTestForCodeCove() {
+        assertTrue(true);
+    }
+
+}
