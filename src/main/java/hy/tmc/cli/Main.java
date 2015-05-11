011df15facde02eec8b2ddd6d8ef98c726d1d52a
Chang <chang.rajani@gmail.com>
2015-05-11 15:27:37 +0300

    Add stub for socket connections

--- src/main/java/hy/tmc/cli/Main.java
+++ src/main/java/hy/tmc/cli/Main.java
@@ -1,22 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hy.tmc.cli;
 
-/**
- *
- * @author chang
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("asdndasnuadsnadsun");
+    public static void main(String[] args) throws IOException {
+        // TODO code all application logic here
+        System.out.println("Hello world!");
+        int portNumber = 3576;
+
+        ServerSocket serverSocket = new ServerSocket(portNumber);
+        Socket clientSocket = serverSocket.accept();
+        PrintWriter out
+                = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+
+        String inputLine, outputLine;
+
+        while ((inputLine = in.readLine()) != null) {
+            outputLine = inputLine+" Recieved";
+            out.println(outputLine);
+            if (outputLine.equals("Bye Recieved")) {
+                break;
+            }
+        }
+
     }
-    
+
 }
