50d1fbdd7002a4fa9630348f7d2be157b1850893
Pihla Toivanen <toivanenpihla@gmail.com>
2015-05-12 13:31:00 +0300

    local server branch

--- src/main/java/hy/tmc/cli/Main.java
+++ src/main/java/hy/tmc/cli/Main.java
@@ -11,25 +11,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         // TODO code all application logic here
-        System.out.println("Hello world!");
-        int portNumber = 3576;
-
-        ServerSocket serverSocket = new ServerSocket(portNumber);
-        Socket clientSocket = serverSocket.accept();
-        PrintWriter out
-                = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(clientSocket.getInputStream()));
-
-        String inputLine, outputLine;
-
-        while ((inputLine = in.readLine()) != null) {
-            outputLine = inputLine+" Recieved";
-            out.println(outputLine);
-            if (outputLine.equals("Bye Recieved")) {
-                break;
-            }
-        }
+        
 
     }
 
