1084b84e89c3c1e8181ccdf556bba46b6b18262b
samutamm <samutamm@gmail.com>
2015-05-13 15:38:31 +0300

    Functionality-method to login-command.

--- src/main/java/hy/tmc/cli/frontend_communication/Commands/Login.java
+++ src/main/java/hy/tmc/cli/frontend_communication/Commands/Login.java
@@ -34,6 +34,11 @@ public class Login extends Command {
         }
     }
 
+    @Override
+    protected void functionality() {
+
+    }
+
     private boolean checkIfAuthenticationOk(String username, String password) {
         return username.equals("matti") && password.equals("meikalainen");
     }
@@ -42,7 +47,7 @@ public class Login extends Command {
     public void setParameter(String key, String value) {
         System.out.println(key);
         System.out.println(value);
-        data.put(key,value);
+        data.put(key, value);
     }
 
     @Override
