fc8bca35c3865c2e1b1e35136786207339ff9db1
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2015-05-13 11:03:37 +0300

    command factory

---
+++ src/main/java/hy/tmc/cli/frontend_communication/Commands/CommandFactory.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hy.tmc.cli.frontend_communication.Commands;
+
+import hy.tmc.cli.frontend_communication.Commands.CommandLineClientCommands.ReplyToPing;
+import hy.tmc.cli.frontend_communication.FrontendListener;
+import hy.tmc.cli.logic.Logic;
+
+/**
+ *
+ * @author ilari
+ */
+public class CommandFactory {
+    
+    public static Command Help(FrontendListener front, Logic back) {
+        return new Help(front, back);
+    }
+    
+    public static Command Echo(FrontendListener front, Logic back) {
+        return new Echo(front, back);
+    }
+    
+    public static Command ReplyToPing(FrontendListener front, Logic back) {
+        return new ReplyToPing(front, back);
+    }
+}
