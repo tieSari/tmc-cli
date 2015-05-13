10db363ff1d7a96d5dbd9c6fec0a2a610dfbf121
Kristian Wahlroos <wakr@cs.helsinki.fi>
2015-05-13 09:52:54 +0300

    getting PID

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -14,8 +14,9 @@ fi
 RESPONSE=eval nc -z -w5 localhost $PORT; RESPONSE=$?
 
 if [ $RESPONSE = 1 ]; then
-  eval nohup java -jar tmc-client.jar &> /dev/null &
-  #$! et saa viimesen background prosessin parametrin
+  eval "(nohup java -jar tmc-client.jar &> /dev/null) &"
+  PID=$!
+  echo $PID
   echo "Server started"
 else
   echo "Server is already running. Not doing anything."
