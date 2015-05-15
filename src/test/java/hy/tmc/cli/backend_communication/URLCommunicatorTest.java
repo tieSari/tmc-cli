dfbddd4f9ce03fd9ce0d8894176706f221fd92e3
samutamm <samutamm@gmail.com>
2015-05-15 14:39:18 +0300

    Refactor.

--- src/test/java/hy/tmc/cli/backend_communication/URLCommunicatorTest.java
+++ src/test/java/hy/tmc/cli/backend_communication/URLCommunicatorTest.java
@@ -3,15 +3,21 @@ package hy.tmc.cli.backend_communication;
 import hy.tmc.cli.backendCommunication.URLCommunicator;
 import org.apache.http.client.HttpClient;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import static org.mockito.Matchers.anyObject;
 import org.mockito.Mockito;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  *
  * @author samutamm
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(URLCommunicator.class)
 public class URLCommunicatorTest {
     
     private HttpClient clientMock;
@@ -22,7 +28,8 @@ public class URLCommunicatorTest {
     
     @Test
     public void createGetMethodWorks() {
-        TestURLCommunicator
+        PowerMockito.mockStatic(URLCommunicator.class);
+        when(URLCommunicator.makeGetRequest(null, null));
     }
     
     
