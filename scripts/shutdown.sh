0456bfcd1cff3c927a30532fa034b4f87318db3d
Jani Luukko <jj.luukko@gmail.com>
2015-05-15 13:05:02 +0300

    Full paths added to bash scripts
    (In order to fix FrontendTest)

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-kill -9 `cat config`
-echo "" > config
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+CONFIGPATH=$DIR
+CONFIGPATH+="/config"
+
+kill -9 `cat $CONFIGPATH`
+echo "" > $CONFIGPATH
