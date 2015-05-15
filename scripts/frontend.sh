a29ba7d27a74df844b8bcbd168381c2243f0f9a7
Jani Luukko <jj.luukko@gmail.com>
2015-05-15 10:10:34 +0300

    Login command implemented
    Pair programmed with @UMTti

--- scripts/frontend.sh
+++ scripts/frontend.sh
@@ -3,42 +3,54 @@
 # echo $@
 # echo $1
 
-function command_help () {
-    declare -A array
-    array[help]=description
-
-    echo "Listing commands..."
-    echo "-----"
-
-    for i in "${!array[@]}"
-    do
-        echo "command: $i"
-        echo "description: ${array[$i]}"
-    done
-
-    return 0;
-}
+#function command_help () {
+#    declare -A array
+#    array[help]=description
+#
+#    echo "Listing commands..."
+#    echo "-----"
+#
+#    for i in "${!array[@]}"
+#    do
+#        echo "command: $i"
+#        echo "description: ${array[$i]}"
+#    done
+#
+#    return 0;
+#}
 
 function command_login () {
     read -p "Username: " username
     read -s  -p "Password: " password
     echo ""
+    login $username $password
     return 0;
 }
 
 function command_default () {
-    command_help
+    send_command $@
     return 0;
 }
 
 # Backend login
 function login () {
+    # login username <kayttajatunnus> password <pw>
+
+    loginstring="login username "
+    loginstring+=$1
+    loginstring+=" password "
+    loginstring+=$2
+
+    send_command $loginstring
+
     return 0;
 
 }
 
 # Backend cmd send
 function send_command () {
+    OUTPUT=$(echo $@ | nc localhost 1234)
+    echo $OUTPUT
     return 0;
 
 }
@@ -47,7 +59,7 @@ function send_command () {
 bash startup.sh
 
 case "$1" in
-    "help") command_help;;
+#    "help") command_help;;
     "login") command_login;;
-    *) command_default;;
+    *) command_default $@;;
 esac
