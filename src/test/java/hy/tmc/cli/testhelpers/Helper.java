834213c36fa583d4c51129a74bd7bcdfc6d77322
Jani Luukko <jj.luukko@gmail.com>
2015-05-15 13:05:40 +0300

    Refactored helper

--- src/test/java/hy/tmc/cli/testhelpers/Helper.java
+++ src/test/java/hy/tmc/cli/testhelpers/Helper.java
@@ -10,19 +10,28 @@ import java.io.InputStreamReader;
  */
 public class Helper {
 
-    public String startCommand(String command, String cliPath) {
-        Process p = null;
+    public ProcessBuilder createProcessBuilder(String command, String cliPath) {
 
         if (cliPath == null) {
-            cliPath = "scipts/frontend.sh";
+            cliPath = "scripts/frontend.sh";
         }
 
+        return new ProcessBuilder("bash", cliPath, command);
+    }
+
+    public String printOutput(String command, String cliPath) {
+
+        Process p = null;
         try {
-            p = new ProcessBuilder("bash", cliPath, command).start();
+            p = createProcessBuilder(command, cliPath).start();
             p.waitFor();
         } catch (Exception e) {
+            System.out.println("prosessin luonti feilas");
             return "";
         }
+
+        System.out.println("luotiin prosessi");
+
         InputStream inputStream = p.getInputStream();
 
         StringBuilder sb = new StringBuilder();
@@ -34,6 +43,9 @@ public class Helper {
         } catch (IOException e) {
             return "";
         }
+        System.out.println("tulos:");
+        System.out.println(sb.toString());
+
         return sb.toString();
     }
 }
