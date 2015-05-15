9f65390b3a236695b775938a5784d4310bcc129c
samutamm <samutamm@gmail.com>
2015-05-15 14:59:21 +0300

    Testing URLCommunicator.

--- src/main/java/hy/tmc/cli/backendCommunication/URLCommunicator.java
+++ src/main/java/hy/tmc/cli/backendCommunication/URLCommunicator.java
@@ -20,7 +20,6 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 /**
--- src/test/java/hy/tmc/cli/backend_communication/URLCommunicatorTest.java
+++ src/test/java/hy/tmc/cli/backend_communication/URLCommunicatorTest.java
@@ -1,36 +1,29 @@
 package hy.tmc.cli.backend_communication;
 
+import hy.tmc.cli.backendCommunication.HTTPResult;
 import hy.tmc.cli.backendCommunication.URLCommunicator;
-import org.apache.http.client.HttpClient;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import static org.mockito.Matchers.anyObject;
-import org.mockito.Mockito;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  *
  * @author samutamm
  */
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(URLCommunicator.class)
+
 public class URLCommunicatorTest {
     
-    private HttpClient clientMock;
-
-    public URLCommunicatorTest() {
-        this.clientMock = mock(HttpClient.class);
+    @Test
+    public void createGetMethodWorksWithValidParams() {
+        HTTPResult result = URLCommunicator.makeGetRequest("https://tmc.mooc.fi/hy", "test:1234");
+        assertEquals(result.getStatusCode(), 200);
     }
     
     @Test
-    public void createGetMethodWorks() {
-        PowerMockito.mockStatic(URLCommunicator.class);
-        when(URLCommunicator.makeGetRequest(null, null));
+    public void createGetMethodDoNotWorkWithoutValidParams() {
+        HTTPResult result = URLCommunicator.makeGetRequest("https://tmc.mooc.fo", "test:1234");
+        assertEquals(result.getStatusCode(), 400);
     }
+    
     
     
 }
