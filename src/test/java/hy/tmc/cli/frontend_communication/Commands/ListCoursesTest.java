eaf434f70391a6632c387d1d5c46afb3b35d1a5b
Kristian Wahlroos <wakr@cs.helsinki.fi>
2015-05-19 12:57:20 +0300

    Fixed some tests

--- src/test/java/hy/tmc/cli/frontend_communication/Commands/ListCoursesTest.java
+++ src/test/java/hy/tmc/cli/frontend_communication/Commands/ListCoursesTest.java
@@ -18,6 +18,7 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(URLCommunicator.class)
 public class ListCoursesTest {
