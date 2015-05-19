778a42cb6e422971771c3a4c2db5ea9cddfec104
Chang <chang.rajani@gmail.com>
2015-05-19 09:43:04 +0300

    Add working mocked tests to JSONParser

--- src/test/java/hy/tmc/cli/backend_communication/JSONParserTest.java
+++ src/test/java/hy/tmc/cli/backend_communication/JSONParserTest.java
@@ -5,38 +5,51 @@ import hy.tmc.cli.backendCommunication.HTTPResult;
 import hy.tmc.cli.backendCommunication.JSONParser;
 import hy.tmc.cli.backendCommunication.URLCommunicator;
 import hy.tmc.cli.testhelpers.ExampleJSON;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.mockito.Mockito.verify;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-    
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(URLCommunicator.class)
 public class JSONParserTest {
-    
-    @Test
-    public void parsesCourseNamesCorrectly() {
-        
+
+    @Before
+    public void setup() {
         PowerMockito.mockStatic(URLCommunicator.class);
-        
+
         HTTPResult fakeResult = new HTTPResult(ExampleJSON.coursesExample, 200, true);
-        
-        
+
         ClientData.setUserData("chang", "paras");
         PowerMockito
-                .when(URLCommunicator.makeGetRequest("http://changonparas.com", ClientData.getFormattedUserData()))
+                .when(URLCommunicator.makeGetRequest(Mockito.anyString(), Mockito.anyString()))
                 .thenReturn(fakeResult);
-                
+    }
+
+    @Test
+    public void parsesCourseNamesCorrectly() {
         String courses = JSONParser.getCourseNames();
-        
         assertTrue(courses.contains("s2014-tira"));
         assertTrue(courses.contains("k2015-ohpe"));
         assertTrue(courses.contains("checkstyle-demo"));
         assertTrue(courses.contains("2014-mooc-no-deadline"));
         assertTrue(courses.contains("c-demo"));
     }
+
+    @Test
+    public void coursesDontContainWeirdNames() {
+        String courses = JSONParser.getCourseNames();
+        assertFalse(courses.contains("Chang"));
+        assertFalse(courses.contains("Ilari"));
+        assertFalse(courses.contains("Pihla"));
+        assertFalse(courses.contains("Kristian"));
+        assertFalse(courses.contains("Samu"));
+        assertFalse(courses.contains("Jani"));
+    }
+
 }
