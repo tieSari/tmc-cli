f8c7e116074fd2623cc796926ebb28ce806aa275
samutamm <samutamm@gmail.com>
2015-05-20 10:49:59 +0300

    Class ready.

--- src/main/java/hy/tmc/cli/Configuration/ConfigurationParser.java
+++ src/main/java/hy/tmc/cli/Configuration/ConfigurationParser.java
@@ -1,16 +1,27 @@
 package hy.tmc.cli.Configuration;
 
 public class ConfigurationParser {
-    
+
     private String[] arguments;
 
+    /**
+     * This class will configure the URL of TMC-server.
+     */
     public ConfigurationParser(String[] arguments) {
         this.arguments = arguments;
         this.configureServerData();
     }
 
     private void configureServerData() {
+        if (arguments.length < 1) {
+            return;
+        }
         String authenticationURL = arguments[0];
+        ServerData.setAuthUrl(authenticationURL);
+        if (arguments.length < 2) {
+            return;
+        }
         String coursesURL = arguments[1];
+        ServerData.setCoursesUrl(coursesURL);
     }
 }
