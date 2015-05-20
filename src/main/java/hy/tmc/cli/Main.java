f7488729845022fad739a72a198a0832424f28cb
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2015-05-20 14:24:26 +0300

    fix main

--- src/main/java/hy/tmc/cli/Main.java
+++ src/main/java/hy/tmc/cli/Main.java
@@ -5,17 +5,26 @@ import hy.tmc.cli.frontend_communication.FrontendListener;
 import hy.tmc.cli.frontend_communication.Server.Server;
 import hy.tmc.cli.logic.Logic;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.lingala.zip4j.exception.ZipException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         
         ZipHandler zip = new ZipHandler("viikko1-Viikko1_004.Muuttujat.zip", ".");
-        zip.unzip();
-        
+        try {
+            zip.unzip();
+            
 //        Logic backend = new Logic();
 //        FrontendListener frontendListener = new Server(1234, backend);
 //        frontendListener.start();
+        }
+        catch (ZipException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("unable to zip");
+        }
 
     }
 
