79235b46464ed2b20057fd94c783cfaa5c2efe9f
samutamm <samutamm@gmail.com>
2015-05-21 14:51:45 +0300

    Frontend now works with finding free port

--- src/test/java/feature/logout/LogoutSteps.java
+++ src/test/java/feature/logout/LogoutSteps.java
@@ -23,6 +23,7 @@ public class LogoutSteps {
 
     @Before
     public void initializeServer() throws IOException {
+        ClientData.clearUserData();
         server = new Server(null);
         port = new ConfigHandler().readPort();
         serverThread = new Thread(server);
