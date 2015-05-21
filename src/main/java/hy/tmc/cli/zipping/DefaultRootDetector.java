5485f2533115fbeeeb4d7f245557323b42412473
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2015-05-21 12:45:13 +0300

    javadoc

--- src/main/java/hy/tmc/cli/zipping/DefaultRootDetector.java
+++ src/main/java/hy/tmc/cli/zipping/DefaultRootDetector.java
@@ -6,6 +6,14 @@ import java.nio.file.Path;
 
 public class DefaultRootDetector implements ProjectRootDetector{
     
+    /**
+     * Determine whether a directory is a project root directory. The default
+     * detector detects Maven and Ant projects. A directory will be considered
+     * root if it contains a pom.xml or build.xml
+     * 
+     * @param path path of the directory
+     * @return true iff the path denotes a directory which is a project root
+     */
     @Override
     public boolean isRootDirectory(Path path) {
         File dir = path.toFile();
