9c06d652787a88ff2c601fbcdb551e7a260d9e05
Jani Luukko <jj.luukko@gmail.com>
2015-05-25 13:14:03 +0300

    added javadoc to main

--- src/main/java/hy/tmc/cli/Main.java
+++ src/main/java/hy/tmc/cli/Main.java
@@ -3,12 +3,15 @@ package hy.tmc.cli;
 import hy.tmc.cli.frontend.communication.FrontendListener;
 import hy.tmc.cli.frontend.communication.server.Server;
 import hy.tmc.cli.logic.Logic;
+
 import java.io.IOException;
 
 public class Main {
 
+    /**
+     * Starts the main program.
+     */
     public static void main(String[] args) throws IOException, InterruptedException {
-
         Logic backend = new Logic();
         FrontendListener frontendListener = new Server(backend);
         frontendListener.start();
