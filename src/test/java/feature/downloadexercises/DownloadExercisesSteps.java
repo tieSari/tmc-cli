e923000d8d70ed880bba780d42a068b63c055df2
Jani Luukko <jj.luukko@gmail.com>
2015-05-26 15:27:34 +0300

    wiremock test fix

--- src/test/java/feature/downloadexercises/DownloadExercisesSteps.java
+++ src/test/java/feature/downloadexercises/DownloadExercisesSteps.java
@@ -96,9 +96,9 @@ public class DownloadExercisesSteps {
                 break;
             }
         }
-        wireMockServer.verify(getRequestedFor(urlEqualTo("/courses/21.json?api_version=7"))
+        verify(getRequestedFor(urlEqualTo("/courses/21.json?api_version=7"))
                 .withHeader("Authorization", equalTo("Basic cGlobGE6anV1aA==")));
-        wireMockServer.verify(getRequestedFor(urlMatching("/exercises/[0-9]+.zip"))
+        verify(getRequestedFor(urlMatching("/exercises/[0-9]+.zip"))
                 .withHeader("Authorization", equalTo("Basic cGlobGE6anV1aA==")));
     }
 
