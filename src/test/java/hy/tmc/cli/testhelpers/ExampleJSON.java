43a6c9cc623d8162ccfae781e89b56f91af7ab09
Jani Luukko <jj.luukko@gmail.com>
2015-05-26 13:40:36 +0300

    use file.seperator in dlexercisessteps

--- src/test/java/hy/tmc/cli/testhelpers/ExampleJSON.java
+++ src/test/java/hy/tmc/cli/testhelpers/ExampleJSON.java
@@ -10,7 +10,6 @@ public class ExampleJSON {
     public static String allCoursesExample = allCoursesExample();
     
     private static String courseExample(){
-        System.out.println(System.getProperty("user.dir"));
         try {
             return FileUtils.readFileToString(new File("src/test/resources/course.json"));
         }
