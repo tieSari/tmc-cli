80998e17403e1eb35ce588d0c9e84820ae74dc06
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2015-05-27 12:50:38 +0300

    builders to help testing

--- src/test/java/hy/tmc/cli/testhelpers/TestResultBuilder.java
+++ src/test/java/hy/tmc/cli/testhelpers/TestResultBuilder.java
@@ -1,6 +1,60 @@
 package hy.tmc.cli.testhelpers;
 
+import com.google.common.collect.ImmutableList;
+import fi.helsinki.cs.tmc.langs.TestResult;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestResultBuilder {
 
+    private String name;
+    private boolean passed;
+    private ArrayList<String> points;
+    private String errorMessage;
+    private ArrayList<String> stackTrace;
+
+    public TestResultBuilder() {
+        this.points = new ArrayList<>();
+        this.stackTrace = new ArrayList<>();
+    }
+
+    public TestResultBuilder withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public TestResultBuilder withPassedStatus(boolean passed) {
+        this.passed = passed;
+        return this;
+    }
+
+    public TestResultBuilder withPoint(String point) {
+        this.points.add(point);
+        return this;
+    }
+    
+    public TestResultBuilder withErrorMessage(String errMsg){
+        this.errorMessage = errMsg;
+        return this;
+    }
+
+    public TestResultBuilder withPoints(List<String> points) {
+        this.points.addAll(points);
+        return this;
+    }
+
+    public TestResultBuilder withStackTrace(List<String> trace) {
+        this.stackTrace.addAll(trace);
+        return this;
+    }
+
+    public TestResultBuilder withStackTraceLine(String line) {
+        this.stackTrace.add(line);
+        return this;
+    }
+
+    public TestResult build() {
+        return new TestResult(name, passed, ImmutableList.copyOf(points), errorMessage,
+                ImmutableList.copyOf(stackTrace));
+    }
 }
