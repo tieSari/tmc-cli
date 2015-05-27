c5afb691274720664762d7f3bfe1f6d80692ed71
Pihla Toivanen <pihla.toivanen@helsinki.fi>
2015-05-27 14:04:52 +0300

    get rid of test code

--- src/main/java/hy/tmc/cli/Main.java
+++ src/main/java/hy/tmc/cli/Main.java
@@ -17,18 +17,18 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         
         
-        ImmutableList<TestResult> tests = tests();
+        /*ImmutableList<TestResult> tests = tests();
         
         RunResult r = new RunResult(TESTS_FAILED, tests, null);
         ResultInterpreter i = new ResultInterpreter();
-        System.out.println(i.interpret(r));
+        System.out.println(i.interpret(r));*/
 
-        //Logic backend = new Logic();
-        //FrontendListener frontendListener = new Server(backend);
-        //frontendListener.start();
+        Logic backend = new Logic();
+        FrontendListener frontendListener = new Server(backend);
+        frontendListener.start();
     }
     
-    private static ImmutableList<TestResult> tests() {
+    /*private static ImmutableList<TestResult> tests() {
         List<TestResult> l = new ArrayList<>();
         List<String> ll = new ArrayList<>();
         ll.add("asdf");
@@ -36,5 +36,5 @@ public class Main {
         l.add(new TestResult("test", true, null, "error", ImmutableList.copyOf(ll)));
         
         return ImmutableList.copyOf(l);
-    }
+    }*/
 }
