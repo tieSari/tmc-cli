9b83052c49e6534b1a329031ea557c224fd2152a
Pihla Toivanen <pihla.toivanen@helsinki.fi>
2015-05-28 13:56:12 +0300

    DownloadExercises command test

---
+++ src/test/java/hy/tmc/cli/frontend_communication/commands/DownloadExercisesTest.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hy.tmc.cli.frontend_communication.commands;
+
+import hy.tmc.cli.frontend.communication.commands.DownloadExercises;
+import hy.tmc.cli.frontend.communication.commands.RunTests;
+import hy.tmc.cli.frontend.communication.server.ProtocolException;
+import hy.tmc.cli.testhelpers.FrontendStub;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author xtoxtox
+ */
+public class DownloadExercisesTest {
+    
+    private FrontendStub front;
+
+    /**
+     * Create FrontendStub. 
+     */
+    @Before
+    public void setup() {
+        front = new FrontendStub();
+    }
+    
+    /**
+     * Check that data checking success.
+     */
+    @Test
+    public void testCheckDataSuccess() {
+        DownloadExercises de = new DownloadExercises(front,null);
+        de.setParameter("pwd", "/home/tmccli/uolevipuistossa");
+        de.setParameter("courseID", "21");
+        try {
+            de.checkData();
+        } catch (ProtocolException p) {
+            fail("testCheckDataSuccess failed");
+        }
+    }
+
+    /**
+     * Check that if user didn't give correct data, data checking fails.
+     */
+    @Test
+    public void testCheckDataFail() {
+        DownloadExercises de = new DownloadExercises(front,null);
+        try {
+            de.checkData();
+            fail("testCheckDataFail should have failed");
+        } catch (ProtocolException p) {
+            return;
+        }
+    }
+    
+    /**
+     * User gives course id that isn't a number and will be informed about it.
+     */
+    @Test
+    public void courseIDNotANumber() {
+        DownloadExercises de = new DownloadExercises(front,null);
+        de.setParameter("pwd", "/home/tmccli/uolevipuistossa");
+        de.setParameter("courseID", "not a number");
+        try {
+            de.checkData();
+            fail("testCheckDataFail should have failed");
+        } catch (ProtocolException p) {
+            return;
+        }
+    }
+}
