7a5b9ac055bbcafb9c82cbac2a394a99beb71288
Kristian Wahlroos <wakr@cs.helsinki.fi>
2015-05-28 12:41:56 +0300

    update

---
+++ src/test/java/hy/tmc/cli/testhelpers/ProjectRootFinderStub.java
@@ -0,0 +1,28 @@
+package hy.tmc.cli.testhelpers;
+
+import hy.tmc.cli.zipping.RootFinder;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ProjectRootFinderStub implements RootFinder {
+
+    private String returnValue;
+
+    public ProjectRootFinderStub() {
+        this.returnValue = "";
+    }
+
+    public String getReturnValue() {
+        return returnValue;
+    }
+
+    public void setReturnValue(String returnValue) {
+        this.returnValue = returnValue;
+    }
+
+    @Override
+    public Path getRootDirectory(Path zipRoot) {
+        return zipRoot;
+    }
+
+}
