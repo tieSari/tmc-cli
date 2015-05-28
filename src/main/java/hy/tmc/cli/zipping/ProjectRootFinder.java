31394bc717de6bebc6e954b39b9442b2917c3d3a
Kristian Wahlroos <wakr@cs.helsinki.fi>
2015-05-28 12:35:03 +0300

    first test for Submitter

--- src/main/java/hy/tmc/cli/zipping/ProjectRootFinder.java
+++ src/main/java/hy/tmc/cli/zipping/ProjectRootFinder.java
@@ -4,26 +4,28 @@ import java.io.File;
 import java.nio.file.Path;
 import java.util.ArrayDeque;
 
-public class ProjectRootFinder {
+public class ProjectRootFinder implements RootFinder {
 
     private final ProjectRootDetector detector;
-    
+
     /**
-     * A helper class that searches for a project root directory. It must be 
-     * given a ProjectRootDetector that corresponds with the project in question.
-     * For example, for a Maven project a DefaultRootDetector can be used.
-     * 
+     * A helper class that searches for a project root directory. It must be given a
+     * ProjectRootDetector that corresponds with the project in question. For example, for a Maven
+     * project a DefaultRootDetector can be used.
+     *
      * @param detector the RootDetector that specifies if a directory is a project root
      */
     public ProjectRootFinder(ProjectRootDetector detector) {
         this.detector = detector;
     }
-    
+
     /**
      * Get the path of the project root directory.
+     *
      * @param zipRoot the path of the extracted zip.
      * @return path of the root, of null if no root was found
      */
+    @Override
     public Path getRootDirectory(Path zipRoot) {
         return breadthFirstSearch(zipRoot);
     }
--- src/test/java/hy/tmc/cli/backendcommunication/CourseSubmitterTest.java
+++ src/test/java/hy/tmc/cli/backendcommunication/CourseSubmitterTest.java
@@ -2,9 +2,11 @@ package hy.tmc.cli.backendcommunication;
 
 import hy.tmc.cli.configuration.ClientData;
 import hy.tmc.cli.domain.Course;
+import hy.tmc.cli.testhelpers.ProjectRootFinderStub;
 import hy.tmc.cli.zipping.DefaultRootDetector;
 import hy.tmc.cli.zipping.ProjectRootFinder;
 import java.io.IOException;
+import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,13 +17,14 @@ public class CourseSubmitterTest {
 
     @Before
     public void setup() {
-        this.courseSubmitter = new CourseSubmitter(new ProjectRootFinder(new DefaultRootDetector()));
+        ProjectRootFinderStub projectRootFinderStub = new ProjectRootFinderStub();
+        this.courseSubmitter = new CourseSubmitter(projectRootFinderStub);
     }
 
     @Test
-    public void testLastDirectory() {
-        String[] names = courseSubmitter.getExerciseName("");
-
+    public void testGetExerciseName() {
+        String[] names = courseSubmitter.getExerciseName("/home/test/ohpe-test/viikko_01");
+        assertEquals("viikko_01", names[names.length - 1]);
     }
 
 }
