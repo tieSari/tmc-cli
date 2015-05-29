860cccbfeb37a55fd58dd4a18dabe380f76f7a8b
Jani Luukko <jj.luukko@gmail.com>
2015-05-29 14:04:18 +0300

    Forgot import in ListCoursesSteps

--- src/test/java/feature/listcourses/ListCoursesSteps.java
+++ src/test/java/feature/listcourses/ListCoursesSteps.java
@@ -6,6 +6,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
