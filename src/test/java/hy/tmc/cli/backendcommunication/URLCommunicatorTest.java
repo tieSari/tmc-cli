023cd42f99c5635cbdb5283f3805fd0711e401b0
Kristian Wahlroos <wakr@cs.helsinki.fi>
2015-05-29 10:46:50 +0300

    refactored post & get

--- src/test/java/hy/tmc/cli/backendcommunication/URLCommunicatorTest.java
+++ src/test/java/hy/tmc/cli/backendcommunication/URLCommunicatorTest.java
@@ -5,9 +5,12 @@ import hy.tmc.cli.backendcommunication.UrlCommunicator;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.containing;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import org.apache.http.HttpResponse;
@@ -19,7 +22,7 @@ public class URLCommunicatorTest {
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule();
-    
+
     @Test
     public void okWithValidParams() {
         stubFor(get(urlEqualTo("/"))
@@ -42,36 +45,35 @@ public class URLCommunicatorTest {
                         .withStatus(400)
                 )
         );
-        
+
         HttpResult result = UrlCommunicator.makeGetRequest("http://127.0.0.1:8080/vaaraurl", "test:1234");
         assertEquals(400, result.getStatusCode());
     }
-    
+
     @Test
     public void notFoundWithoutValidParams() {
-        // next will fail, becouse wiremock does not accept headers like that.
         HttpResult result = UrlCommunicator.makeGetRequest("http://127.0.0.1:8080/", "ihanvaaraheaderi:1234");
         assertEquals(403, result.getStatusCode());
     }
 
     @Test
-    public void createGetRequest() throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        stubFor(get(urlEqualTo("/"))
-                .withHeader("Authorization", containing("Basic dGVzdDoxMjM0"))
+    public void httpPostAddsFileToRequest() throws IOException {
+        stubFor(post(urlEqualTo("/kivaurl"))
+               // .withHeader("Authorization", containing("Basic dGVzdDoxMjM0"))
                 .willReturn(
                         aResponse()
                         .withStatus(200)
                 )
         );
-        
-        Method createMethod = UrlCommunicator.class.getDeclaredMethod("createAndExecuteGet", String.class, String[].class);
-        createMethod.setAccessible(true);
-        Object instance = UrlCommunicator.class.newInstance();
-        HttpResponse result = (HttpResponse) createMethod.invoke(instance, "http://127.0.0.1:8080/", new String[]{
-            "test:1234"
-        });
-
-        HttpResponse httpResponse = (HttpResponse) result;
-        assertEquals(200, httpResponse.getStatusLine().getStatusCode());
+        File testFile = new File("testResources/test.zip");
+        System.out.println(testFile.getAbsolutePath());
+        HttpResult result = UrlCommunicator.makePostWithFile(testFile, "http://127.0.0.1:8080/kivaurl");
+        System.out.println(result.getData());
     }
+
+    @Test
+    public void badGetRequestIsCatched() {
+
+    }
+
 }
