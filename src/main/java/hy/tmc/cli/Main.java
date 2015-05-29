598f5f98da9ff2bc7bff4b3161e5200eb0d92258
Jani Luukko <jj.luukko@gmail.com>
2015-05-29 13:23:35 +0300

    Main checkstyle-fixed

--- src/main/java/hy/tmc/cli/Main.java
+++ src/main/java/hy/tmc/cli/Main.java
@@ -3,10 +3,14 @@ package hy.tmc.cli;
 import hy.tmc.cli.frontend.FrontendListener;
 import hy.tmc.cli.frontend.communication.server.Server;
 import hy.tmc.cli.logic.Logic;
+
 import java.io.IOException;
 
 public class Main {
 
+    /**
+     * Starts the server.
+     */
     public static void main(String[] args) throws IOException, InterruptedException {
         Logic backend = new Logic();
         FrontendListener frontendListener = new Server(backend);
