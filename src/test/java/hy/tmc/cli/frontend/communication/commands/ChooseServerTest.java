cab8c63535d9c239467798382e953c0ab73b108a
Pihla Toivanen <pihla.toivanen@helsinki.fi>
2015-05-29 12:34:35 +0300

    hy.tmc.cli.frontend.communication.commands checkstyle

--- src/test/java/hy/tmc/cli/frontend/communication/commands/ChooseServerTest.java
+++ src/test/java/hy/tmc/cli/frontend/communication/commands/ChooseServerTest.java
@@ -1,20 +1,22 @@
 package hy.tmc.cli.frontend.communication.commands;
 
-import hy.tmc.cli.configuration.ConfigHandler;
-import hy.tmc.cli.frontend.communication.commands.ChooseServer;
-import hy.tmc.cli.frontend.communication.server.ProtocolException;
-import hy.tmc.cli.logic.Logic;
-import hy.tmc.cli.testhelpers.FrontendStub;
-import java.io.File;
-import java.io.IOException;
-import org.apache.commons.io.FileUtils;
-import org.junit.After;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import hy.tmc.cli.configuration.ConfigHandler;
+import hy.tmc.cli.frontend.communication.server.ProtocolException;
+import hy.tmc.cli.logic.Logic;
+import hy.tmc.cli.testhelpers.FrontendStub;
+
+import org.apache.commons.io.FileUtils;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
 
 public class ChooseServerTest {
 
@@ -35,7 +37,7 @@ public class ChooseServerTest {
     }
     
     @After
-    public void teardown(){
+    public void teardown() {
         new File(path).delete();
     }
 
@@ -51,30 +53,28 @@ public class ChooseServerTest {
         try {
             String propFile = FileUtils.readFileToString(new File(path));
             assertTrue(propFile.contains("tmc.ebin.fi"));
-        }
-        catch (IOException ex) {
+        } catch (IOException ex) {
             fail("unable to read propertiesfile");
         }
     }
     
     @Test (expected = ProtocolException.class)
-    public void throwsExceptionWithoutData() throws ProtocolException{
+    public void throwsExceptionWithoutData() throws ProtocolException {
         chooser.checkData();
     }
     
     @Test
-    public void correctURLisAccepted() {
+    public void correctUrlisAccepted() {
         chooser.setParameter("tmc-server", "http://tmc.mooc.fi");
         try {
             chooser.checkData();
-        }
-        catch (ProtocolException ex) {
+        } catch (ProtocolException ex) {
             fail("checkData threw exception");
         }
     }
     
     @Test (expected = ProtocolException.class)
-    public void incorrectURLThrowsException() throws ProtocolException {
+    public void incorrectUrlThrowsException() throws ProtocolException {
         chooser.setParameter("tmc-server", "lak3jf02ja3fji23j");
         chooser.checkData();
     }
