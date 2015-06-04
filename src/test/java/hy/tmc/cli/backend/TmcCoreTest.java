d6216e278cdf926e03503b87964bda349047fe7a
Kristian Wahlroos <wakr@cs.helsinki.fi>
2015-06-04 14:18:33 +0300

    Fixed pr comments

--- src/test/java/hy/tmc/cli/backend/TmcCoreTest.java
+++ src/test/java/hy/tmc/cli/backend/TmcCoreTest.java
@@ -1,23 +1,25 @@
 package hy.tmc.cli.backend;
 
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import hy.tmc.cli.backend.communication.UrlCommunicator;
 import hy.tmc.cli.frontend.FrontendListener;
 import hy.tmc.cli.frontend.communication.commands.Command;
 import hy.tmc.cli.frontend.communication.commands.CommandFactory;
 import hy.tmc.cli.testhelpers.FrontendStub;
-import java.util.HashMap;
-import java.util.Map;
-import static org.junit.Assert.assertTrue;
+import org.mockito.Mockito;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.mockito.Matchers.anyString;
-import org.mockito.Mockito;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(CommandFactory.class)
@@ -51,58 +53,37 @@ public class TmcCoreTest {
 
     }
 
-    /**
-     * Test of login method, of class TmcCore.
-     */
     @Test
     public void testLogin() {
         assertTrue(tmcCore.login("mock", "isbest"));
 
     }
 
-    /**
-     * Test of logout method, of class TmcCore.
-     */
     @Test
     public void testLogout() {
         assertTrue(tmcCore.logout());
     }
 
-    /**
-     * Test of selectServer method, of class TmcCore.
-     */
     @Test
     public void testSelectServer() {
         assertTrue(tmcCore.selectServer("myServer"));
     }
 
-    /**
-     * Test of downloadExercises method, of class TmcCore.
-     */
     @Test
     public void testDownloadExercises() {
         assertTrue(tmcCore.downloadExercises("asd", "5"));
     }
 
-    /**
-     * Test of help method, of class TmcCore.
-     */
     @Test
     public void testHelp() {
         assertTrue(tmcCore.help());
     }
 
-    /**
-     * Test of listCourses method, of class TmcCore.
-     */
     @Test
     public void testListCourses() {
         assertTrue(tmcCore.listCourses());
     }
 
-    /**
-     * Test of listExercises method, of class TmcCore.
-     */
     @Test
     public void testListExercises() {
         assertTrue(tmcCore.listExercises());
