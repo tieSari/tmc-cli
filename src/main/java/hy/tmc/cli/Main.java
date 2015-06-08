75744940461fa8e22a5ad203267e6ea02cdd7cef
Kristian Wahlroos <wakr@cs.helsinki.fi>
2015-06-08 14:59:54 +0300

    Started scheduler

--- src/main/java/hy/tmc/cli/Main.java
+++ src/main/java/hy/tmc/cli/Main.java
@@ -1,7 +1,7 @@
 package hy.tmc.cli;
 
-import hy.tmc.cli.frontend.FrontendListener;
-import hy.tmc.cli.frontend.communication.server.Server;
+import hy.tmc.cli.backend.communication.StatusPoller;
+import hy.tmc.cli.synchronization.TmcServiceScheduler;
 
 import java.io.IOException;
 
@@ -9,12 +9,17 @@ public class Main {
 
     /**
      * Starts the main program.
+     *
      * @param args arguments.
      * @throws java.io.IOException if there is an error while reading user input.
      * @throws java.lang.InterruptedException if server is interrupted. Starts the server.
      */
     public static void main(String[] args) throws IOException, InterruptedException {
-        FrontendListener frontendListener = new Server();
-        frontendListener.start();
+      //  FrontendListener frontendListener = new Server();
+        // frontendListener.start();
+   
+        StatusPoller poller = new StatusPoller();
+        new TmcServiceScheduler().addService(poller).start();
+
     }
 }
--- src/main/java/hy/tmc/cli/backend/communication/StatusPoller.java
+++ src/main/java/hy/tmc/cli/backend/communication/StatusPoller.java
@@ -6,6 +6,9 @@ import java.util.concurrent.TimeUnit;
 
 public class StatusPoller extends AbstractScheduledService {
 
+    
+    
+    
     @Override
     protected void runOneIteration() throws Exception {
         System.out.println("Jee from cron-like");
@@ -13,7 +16,11 @@ public class StatusPoller extends AbstractScheduledService {
 
     @Override
     protected Scheduler scheduler() {
-        return newFixedRateSchedule(0, 30, TimeUnit.SECONDS);
+        return newFixedRateSchedule(0, 5, TimeUnit.SECONDS);
+    }
+    
+    private void checkReviews(){
+        
     }
 
 }
