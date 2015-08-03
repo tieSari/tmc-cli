e0c9c1b01677edd52509b76b21e0be401c66a57b
Pihla Toivanen <toivanenpihla@gmail.com>
2015-08-03 15:06:44 +0300

    langs added to before_install

--- before_install.sh
+++ before_install.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/rage/tmc-core.git
 mvn clean install -U -Dmaven.test.skip=true -f tmc-core/pom.xml
-
+git clone https://github.com/tmc-langs.git
+mvn clean install -U -Dmaven.test.skip=true -f tmc-langs/pom.xml
