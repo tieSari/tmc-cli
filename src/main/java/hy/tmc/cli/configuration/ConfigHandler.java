b7e7b7c2be204553e83bedcb408aa2f3e4a63307
Pihla Toivanen <UMTti@users.noreply.github.com>
2015-08-05 14:14:40 +0300

    Update ConfigHandler.java

--- src/main/java/hy/tmc/cli/configuration/ConfigHandler.java
+++ src/main/java/hy/tmc/cli/configuration/ConfigHandler.java
@@ -60,7 +60,11 @@ public class ConfigHandler {
     private Properties getProperties() {
         Properties prop = new Properties();
         try {
-            InputStream inputStream = new FileInputStream(new File(configFilePath));
+            if (!propertyFile.exists()) {
+-                propertyFile.createNewFile();
+-                return prop;
+-           }
+-           InputStream inputStream = new FileInputStream(propertyFile);
             prop.load(inputStream);
         } catch (IOException e) {
             System.err.println(e.getMessage());
