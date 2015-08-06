e9ee6511c4f9dd51d4687b5c98efa1a442fed297
Pihla Toivanen <toivanenpihla@gmail.com>
2015-08-06 13:51:13 +0300

    index

--- before_install.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/rage/tmc-langs.git
-mvn clean install -U -Dmaven.test.skip=true -f tmc-langs/pom.xml
-git clone https://github.com/rage/tmc-core.git
-mvn clean install -U -Dmaven.test.skip=true -f tmc-core/pom.xml
--- src/test/resources/features/logout.feature
+++
@@ -1,7 +0,0 @@
-Feature: Logout
-    User can logout.
-
-    Scenario: Logout when logged in.
-        Given logout command.
-        When user sees message "cleared"
-        Then user data should be cleared.
