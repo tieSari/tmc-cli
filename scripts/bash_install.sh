3c162ae78dd85249802174eadca51154c833340b
Pihla Toivanen <toivanenpihla@gmail.com>
2015-08-07 11:08:50 +0300

    Checks that all deps exist in computer

--- scripts/bash_install.sh
+++ scripts/bash_install.sh
@@ -1,13 +1,19 @@
 #bin/bash
 
-deps[0] = 'nohup'
-deps[1] = 'pgrep'
-deps[2] = 'netcat'
-deps[3] = 'ps'
-deps[4] = 'sed'
-deps[5] = 'trap'
-deps[6] = 'kill'
-deps[7] = 'echo'
+
+Deps=('nohup' 'pgrep' 'netcat' 'ps' 'sed' 'trap' 'kill' 'echo');
+
+command_not_exists(){
+	!(hash "$1" 2>/dev/null);
+}
+
+for i in "${Deps[@]}"
+do
+	:
+	if command_not_exists $i ; then
+		printf "%s is required but not installed in your computer. \n" $i;
+	fi
+done
 
 cd $( dirname "${BASH_SOURCE[0]}" )
 TMCDIR=$(pwd)
