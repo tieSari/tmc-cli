69dedbc12c3d52a9b55658ee6663314f35443487
Pihla Toivanen <toivanenpihla@gmail.com>
2015-08-07 11:26:47 +0300

    install script

--- scripts/bash_install.sh
+++ scripts/bash_install.sh
@@ -1,6 +1,25 @@
 #bin/bash
 
-cd $( dirname "${BASH_SOURCE[0]}" )
+
+Deps=('nohup' 'pgrep' 'netcat' 'ps' 'sed' 'trap' 'kill' 'echo' 'curl');
+
+command_not_exists(){
+	!(hash "$1" 2>/dev/null);
+}
+
+for i in "${Deps[@]}"
+do
+	:
+	if command_not_exists $i ; then
+		printf "%s is required but not installed in your computer. \n" $i;
+	fi
+done
+
+curl -O http://ptoivanen.users.paivola.fi/tmc-cli-release.zip
+unzip tmc-cli-release.zip
+cd tmc-cli-release
+
+#cd $( dirname "${BASH_SOURCE[0]}" )
 TMCDIR=$(pwd)
 
 echo -e '\n### Added by TestMyCode Commandline client' >> $HOME/.bashrc
