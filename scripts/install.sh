610ce218b651921b7e4a6a0d53398f88ab056192
Pihla Toivanen <UMTti@users.noreply.github.com>
2015-08-07 12:35:29 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -27,4 +27,4 @@ echo -e '\n### Added by TestMyCode Commandline client' >> $HOME/.bashrc
 echo "export PATH=\"$TMCDIR:\$PATH\"" >> $HOME/.bashrc
 source $HOME/.bashrc
 
-echo Install finished. Type "tmc" to get started.
+echo Install finished. Type "tmc" to get started. Usage instructions and support information available in http://rage.github.io/tmc-cli/
